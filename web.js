41073f8b1a28696207aa2a9bb210bebf76705d6b
Jim Maioriello <jim@drjeckyl.net>
2013-07-09 19:32:18 +0000

    modified web.js

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
-
 var app = express.createServer(express.logger());
 
+var fs = require('fs');
+var buff = new Buffer(fs.readFileSync("index.html"));
+//console.log(buff.toString());
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  //response.send('Hello World 2!');
+  response.send(buff.toString())
 });
 
 var port = process.env.PORT || 5000;
